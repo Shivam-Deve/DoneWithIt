47f942b8cce42f221842afa6646e4eea112cc590
Shivam <smshivammishra1998@gmail.com>
2022-12-29 13:45:07 +0530

    layout done

--- App.js
+++ App.js
@@ -1,42 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Image, Alert, Platform, TouchableWithoutFeedback, TouchableOpacity, Button } from 'react-native';
-import logError from 'react-native/Libraries/Utilities/logError';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks'
+import { StyleSheet, Dimensions, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  // console.log("Dimensons of screen are ", Dimensions.get('window'));
+  // console.log(useDeviceOrientation(), useDimensions());
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Text numberOfLines={1} onPress={() => console.log("Text pressed")}>
-        This is a long text to show how "numberoflines" property of text field works work.
-      </Text>
-      {/* <Image source={require('./assets/icon.png')} /> */}
-      <TouchableOpacity onPress={() => console.log("Image is pressed")} onLongPress={() => console.log("Image is pressed and hold")}>
-        <Image
-          blurRadius={2}
-          fadeDuration={1000}
-          source={{
-            uri: "https://picsum.photos/200/300",
-            height: 300,
-            width: 200
-          }}
-        />
-      </TouchableOpacity>
-      {/* TouchableNativeFeedback works on android and it works on color backgroundColor */}
-      <Button color="green" title="Click Me" onPress={() => Alert.alert("My Title", "My message", [
-        { text: "Yes", onPress: () => console.log("Yes") },
-        { text: "No", onPress: () => console.log("No") }
-      ]
-      )} />
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={{
+      backgroundColor: "#fff",
+      width: '100%',
+      height: '30%',
+      flex: 1,
+      flexDirection: "row",
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignContent: 'center',
+      flexWrap: "wrap",
+      // flexBasis: 100,
+      // flexGrow: 1,
+      // flexShrink:1
+    }}>
+      <View style={{ backgroundColor: "goldenrod", width: 100, height: 200 }} />
+      <View style={{ backgroundColor: "darkslategray", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "lightgreen", height: 100, width: 100, top: 20, left: 20, position: 'relative' }} />
+      <View style={{ backgroundColor: "lightyellow", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "orange", height: 100, width: 100 }} />
+      <View style={{ backgroundColor: "gray", height: 100, width: 100, position: 'absolute', top: 20 }} />
+      {/* we can also use flex with number or percentage instead of height and width */}
+    </View>
   );
 }
-const containerStyle = { backgroundColor: 'darkslategray' }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    justifyContent: "center",
-    alignItems: 'center'
-  },
-});
+
